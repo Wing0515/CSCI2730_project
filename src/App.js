3104a42f9e48dd006ac711091c59f2bd2e7ee2eb
Anson Lam <ansonlk48@gmail.com>
2023-12-08 16:54:48 +0800

    feat: fe

--- src/App.js
+++ src/App.js
@@ -7,18 +7,74 @@ import Section from './components/Section'
 import Product from './components/Product'
 
 // ABIs
-import CUHKon from './abis/CUHKon.json'
+import CUHKon from './abis/Cuhkon.json'
 
 // Config
 import config from './config.json'
 
 function App() {
+  const [provider, setProvider] = useState(null)
+  const [cuhkon, setCuhkon] = useState(null)
+
+  const [account, setAccount] = useState(null)
+
+  const [electronics, setElectronics] = useState(null)
+  const [clothing, setClothing] = useState(null)
+  const [toys, setToys] = useState(null)
+
+  const [item, setItem] = useState({})
+  const [toggle, setToggle] = useState(false)
+
+  const togglePop = (item) => {
+    setItem(item)
+    toggle ? setToggle(false) : setToggle(true)
+  }
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+    const network = await provider.getNetwork()
+
+    const cuhkon = new ethers.Contract(config[network.chainId].cuhkon.address, CUHKon, provider)
+    setCuhkon(cuhkon)
+
+    const items = []
+
+    for (var i = 0; i < 9; i++) {
+      const item = await cuhkon.items(i + 1)
+      items.push(item)
+    }
+
+    const electronics = items.filter((item) => item.category === 'electronics')
+    const clothing = items.filter((item) => item.category === 'clothing')
+    const toys = items.filter((item) => item.category === 'toys')
+
+    setElectronics(electronics)
+    setClothing(clothing)
+    setToys(toys)
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
 
   return (
     <div>
+      <Navigation account={account} setAccount={setAccount} />
 
-      <h2>Welcome to CUHKon</h2>
+      <h2>CUHKon Best Sellers</h2>
 
+      {electronics && clothing && toys && (
+        <>
+          <Section title={"Clothing & Jewelry"} items={clothing} togglePop={togglePop} />
+          <Section title={"Electronics & Gadgets"} items={electronics} togglePop={togglePop} />
+          <Section title={"Toys & Gaming"} items={toys} togglePop={togglePop} />
+        </>
+      )}
+
+      {toggle && (
+        <Product item={item} provider={provider} account={account} cuhkon={cuhkon} togglePop={togglePop} />
+      )}
     </div>
   );
 }
